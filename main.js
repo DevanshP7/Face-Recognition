8541f2d1189b71785de9f6891faeeff912967e4f
DevanshP7 <79050720+DevanshP7@users.noreply.github.com>
2021-12-15 20:54:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ Webcam.set({
 
 });
 
-camera = document.getElementById("camera");
+camera = document.getElementById( "camera" );
 
 Webcam.attach('#camera');
 
@@ -15,7 +15,7 @@ function capture_image(){
 
     Webcam.snap(function(data_uri){
 
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'
+        document.getElementById( "result" ).innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'
 
     });
 
@@ -23,10 +23,33 @@ function capture_image(){
 
 console.log('ml5 Version: ', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/PDFHqv91z/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/PDFHqv91z/model.json',modelLoaded);
 
 function modelLoaded(){
 
-    console.log('Model loaded!');
+    console.log( 'Model loaded!' );
 
+}
+
+function identify_image(){
+
+    image = document.getElementById( "captured_image" );
+    classifier.classify(image, gotResult);
+
+}
+
+function gotResult(error, results){
+    if (error){
+
+        console.error(error);
+
+    } else {
+
+        console.log(results);
+        accuracy = results[0].confidence.toFixed(2) * 100;
+        document.getElementById( "person" ).innerHTML = results[0].label;
+        document.getElementById( "accuracy" ).innerHTML = accuracy.toFixed(0) + '%';
+        
+
+    }
 }
